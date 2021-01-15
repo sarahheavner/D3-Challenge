ccaa8c12a1261de6577468facd36669ac5bbc661
Sarah Heavner <sheavner@gmail.com>
2021-01-14 17:55:28 -0700

    added svg chart parameters and append to html

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,4 +1,28 @@
-// @TODO: YOUR CODE HERE!
+//define SVG area
+var svgWidth = 600;
+var svgHeight = 400;
+
+//define chart's margins
+var chartMargin = {
+    top: 30,
+    bottom: 30,
+    right: 30,
+    left: 30
+};
+
+//define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.right - chartMargin.left;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+//select body, append SVG area to it, and set the dimensions
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight);
+
+
+
 
 //load data from csv file
 d3.csv("assets/data/data.csv").then(function(healthData) {
