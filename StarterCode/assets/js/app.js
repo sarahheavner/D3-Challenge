f3ad86b6a64c924f20906689ee5e9576347aacef
Sarah Heavner <sheavner@gmail.com>
2021-01-14 17:36:31 -0700

    read csv and convert data to integers

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,30 @@
 // @TODO: YOUR CODE HERE!
+
+//load data from csv file
+d3.csv("assets/data/data.csv").then(function(healthData) {
+
+    //print data in console
+    console.log(healthData);
+
+    //convert all numerical data from string to integer
+    healthData.forEach(function(data) {
+        data.id = +data.id;
+        data.poverty = +data.poverty;
+        data.povertyMoe = +data.povertyMoe;
+        data.age = +data.age;
+        data.ageMoe = +data.ageMoe;
+        data.income = +data.income;
+        data.incomeMoe = +data.incomeMoe;
+        data.healthcare = +data.healthcare;
+        data.healthcareLow = +data.healthcareLow;
+        data.healthcareHigh = +data.healthcareHigh;
+        data.obesity = +data.obesity;
+        data.obesityLow = +data.obesityLow;
+        data.obesityHigh = +data.obesityHigh;
+        data.smokes = +data.smokes;
+        data.smokesLow = +data.smokesLow;
+        data.smokesHigh = +data.smokesHigh;
+    });
+
+});
+
