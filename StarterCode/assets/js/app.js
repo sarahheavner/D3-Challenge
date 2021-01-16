1a97f883fcb7eefefce029a75aef7b0e6b76c264
Sarah Heavner <sheavner@gmail.com>
2021-01-15 18:10:25 -0700

    added tooltip and began axis label

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,6 +1,6 @@
 //define SVG area
 var svgWidth = 960;
-var svgHeight = 500;
+var svgHeight = 600;
 
 //define chart's margins
 var chartMargin = {
@@ -55,7 +55,7 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
 
     //create scale functions
     var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(healthData, d => d.poverty)])
+    .domain([10, d3.max(healthData, d => d.poverty)])
     .range([0, chartWidth]);
 
     var yLinearScale = d3.scaleLinear()
@@ -82,11 +82,45 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
         .append("circle")
         .attr("cx", d => xLinearScale(d.poverty))
         .attr("cy", d => yLinearScale(d.healthcare))
-        .attr("r", "15")
+        .attr("r", "10")
         .attr("fill", "blue")
         .attr("opacity", ".5");
 
-    
+
+    //initialize tooltip
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.state}<br>Poverty: ${d.poverty}%<br>Lacks Healthcare: ${d.healthcare}%`)
+        });
+
+    //create tooltip in chart
+    chartGroup.call(toolTip);
+
+    //create event listeners to display and hide the tooltip
+    circlesGroup.on("click", function(data) {
+        toolTip.show(data, this);
+        })
+        // onmouseout event
+        .on("mouseout", function(data) {
+            toolTip.hide(data);
+    });
+
+    //create axes labels
+    chartGroup.append("text")
+        .attr("transform","rotate(-90)")
+        .attr("y", 0 - chartMargin.left)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare %");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty %")
+
     
 
 
